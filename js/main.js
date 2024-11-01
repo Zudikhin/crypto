b65affa2d5af2f764690f4e17240f946b7e53f5f
zudikhin <uzudihin@gmail.com>
2024-11-01 10:04:33 +0300

    test

--- js/main.js
+++ js/main.js
@@ -14,5 +14,58 @@ $(document).ready(function() {
     $(".footer_btn").click(function() {
         $('html,body').animate({scrollTop:0}, 1000);
     });
+
+    $('.main_mob_wrap_slider').slick({
+        dots: true,
+        infinite: true,
+        autoplay: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
+
+    let rotationAngle = 0;
+    
+    $('.main_desktop_mid_slider').slick({
+        dots: true,
+        infinite: true,
+        autoplay: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    }).on('beforeChange', function() {
+        rotationAngle += 90;
+        $('.main_preloader').css({
+            'transform': `rotate(${rotationAngle}deg)`,
+            'transition': 'transform 0.5s ease'
+        });
+    });
+
+    $(".instruction_wrap_title").click(function() {
+        $(".instruction_wrap").toggleClass("active");
+        $(".instruction_wrap_body").slideToggle();
+    });
+
+    $('.advantage').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        arrows: false,
+        autoplay: true,
+        centerMode: true,
+        variableWidth: true
+    });
+
+    $('.home_review_slider').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        arrows: false,
+        autoplay: true,
+        centerMode: true,
+        variableWidth: true
+    });
  
 });
